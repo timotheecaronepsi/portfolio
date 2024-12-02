1efe0815067e71c22510e8050bf1d102dadb42eb
Timothée <timothee.caron@ecoles-epsi.net>
2024-12-02 01:03:44 +0100

    Modification de projet

--- src/Entity/Projet.php
+++ src/Entity/Projet.php
@@ -23,6 +23,9 @@ class Projet
     #[ORM\Column(length: 255)]
     private ?string $periode = null;
 
+    #[ORM\Column(length: 100)]
+    private ?string $image = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -60,6 +63,18 @@ class Projet
     public function setPeriode(string $periode): static
     {
         $this->periode = $periode;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
 
         return $this;
     }
