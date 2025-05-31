fc28de6027dce847e1d10858bfeecd80b99941d2
Timothée <timothee.caron@ecoles-epsi.net>
2025-05-31 16:58:00 +0200

    Ajout de la justification des compétences des stages

--- src/Entity/CompetenceStage.php
+++ src/Entity/CompetenceStage.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\CompetenceStageRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CompetenceStageRepository::class)]
@@ -18,6 +19,12 @@ class CompetenceStage
 
     #[ORM\ManyToOne(inversedBy: 'competenceStages')]
     private ?Competence $Ncompetencesstages = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $justification_text = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $justification_img = null;
 
     public function getId(): ?int
     {
@@ -44,6 +51,30 @@ class CompetenceStage
     public function setNcompetencesstages(?Competence $Ncompetencesstages): static
     {
         $this->Ncompetencesstages = $Ncompetencesstages;
+
+        return $this;
+    }
+
+    public function getJustificationText(): ?string
+    {
+        return $this->justification_text;
+    }
+
+    public function setJustificationText(?string $justification_text): static
+    {
+        $this->justification_text = $justification_text;
+
+        return $this;
+    }
+
+    public function getJustificationImg(): ?string
+    {
+        return $this->justification_img;
+    }
+
+    public function setJustificationImg(?string $justification_img): static
+    {
+        $this->justification_img = $justification_img;
 
         return $this;
     }
